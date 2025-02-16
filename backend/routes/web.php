aaca1cfdfb2cefb122d4ded2286df93d270c4725
vinay soni <vinaysoni.work@gmail.com>
2025-02-16 21:02:37 +0530

    Dynamic Dashboard - Completed

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
+    Route::get('/dashboard/data', [DashboardController::class, 'dashboardData'])->name('dashboard.data');
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::get('/profile/password', [ProfileController::class, 'passwordEdit'])->name('profile.password.edit');
